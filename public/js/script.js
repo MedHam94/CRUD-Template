8a65335d16e44a87b4f56658210d2e7d1baf5988
MedHam <hammas.m.mh@gmail.com>
2023-10-18 19:08:08 +0100

    make the logic of a carousel

--- public/js/script.js
+++ public/js/script.js
@@ -1,31 +1,20 @@
 // control Image Carousel
-const carouselImages = document.querySelectorAll("#carousel img");
-const totalImages = carouselImages.length + 1;
-const intervalTime = 3000; //3 seconde
-let currentImageIndex = 0;
-
-function showImages(index) {
-  for (let i = 0; i < carouselImages.length; i++) {
-    if (i === index) {
-      carouselImages[i].style.display = "block";
-    } else {
-      carouselImages[i].style.display = "none";
-    }
-  }
-}
-
-function nextImage() {
-  currentImageIndex = (currentImageIndex + 1) % totalImages;
-  showImages(currentImageIndex);
-}
-
 function startCarousel() {
-  setInterval(() => {
-    nextImage();
-  }, intervalTime);
-}
+  let activeImage = 0;
+  const image = document.querySelectorAll("#carousel img");
 
-showImages(0);
+  function cycleImages() {
+    if (!images[activeImage]) {
+      clearInterval(intervalId);
+      return;
+    }
+    image[activeImage].classList.remove("active");
+    activeImage = (activeImage + 1) % images.length;
+    image[activeImage].classList.add("active");
+  }
+
+  let intervalId = setInterval(cycleImages, 3000);
+}
 // Handle Edit Requests
 
 // Handle Delete Request
